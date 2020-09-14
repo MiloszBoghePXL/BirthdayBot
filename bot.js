716be0f7de8ee899ce4a53f03e6c8e7413705475
MiloszBoghePXL <11800460@student.pxl.be>
2020-09-14 19:44:36 +0200

    config

--- bot.js
+++ bot.js
@@ -1,20 +1,49 @@
 const Discord = require('discord.js');
-const config = require("./config.json");
+//const config = require("./config.json");
 
 const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
+    client.user.setActivity(`Serving Nunu :)`);
 });
 
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-        message.reply('pong');
+client.on("message", async message => {
+    if(message.author.bot) return;
+
+    switch(message.content.toLowerCase()) {
+        case "bday set":
+
+            break;
+        case "bday next":
+
+            break;
+        case "bday list":
+
+            break;
+        case "bday reset":
+
+            break;
+        default:
+            break;
     }
 });
 
+function set(){
 
-// THIS  MUST  BE  THIS  WAY
+}
+
+function next(){
+
+}
+
+function list(){
+
+}
+
+function reset(){
+
+}
 
 client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
